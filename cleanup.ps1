f6a4d35d4ec0fec8aca70bfe0b6d38318536216b
Emanuele Ballarin <emanuele@ballarin.cc>
2022-10-08 05:02:46 +0200

    Update | produce new PDF within CI/CD
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,10 +1,4 @@
 #!/usr/bin/env pwsh
 cd "$HOME/repositories/carso-msc-thesis"
-Remove-Item * -Include *.aux
-Remove-Item * -Include *.bbl
-Remove-Item * -Include *.log
-Remove-Item * -Include *.out
-Remove-Item * -Include *.blg
-Remove-Item * -Include *.toc
-Remove-Item * -Include *.gz
-Remove-Item * -Include *.pdf
+latexmk -C
+Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf
---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd "$HOME/repositories/carso-msc-thesis"
+latexmk -C
+rm -f ./*.aux ./*.bbl ./*.log ./*.out ./*.blg ./*.toc ./*.gz ./*.pdf
