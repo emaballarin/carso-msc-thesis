1f833411b6ab69ce61a16d8b74306e3bffb272e6
Emanuele Ballarin <emanuele@ballarin.cc>
2022-10-14 20:23:45 +0200

    Test autoserve.ps1
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

---
+++ autoserve.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+cd "$HOME/repositories/carso-msc-thesis"
+latexmk -C
+Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf
+latexmk -pdf thesis.tex
+git add -A
+git commit --all
+gitpush
+ssh raspicf /bin/bash /home/emaballarin/HTTPSERVE/CDN/scheduled_from_outside.sh
