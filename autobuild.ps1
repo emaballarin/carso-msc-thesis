b86bfb619b59df6eb2c77c05c22368db7996ae54
Emanuele Ballarin <emanuele@ballarin.cc>
2022-10-23 06:10:28 +0200

    Update!
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- autobuild.ps1
+++ autobuild.ps1
@@ -1,6 +1,9 @@
 #!/usr/bin/env pwsh
 cd "$HOME/repositories/carso-msc-thesis"
 latexmk -C
-Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf,*.xmpi,*.xmpdata
-latexmk -pdf thesis.tex
+Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
+latexmk -interaction=nonstopmode -pdf thesis.tex
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
+latexmk -interaction=nonstopmode -pdf slides.tex
+lualatex -synctex=1 -interaction=nonstopmode slides.tex
+Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
--- autobuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd "$HOME/repositories/carso-msc-thesis"
-latexmk -C
-rm -f ./*.aux ./*.bbl ./*.log ./*.out ./*.blg ./*.toc ./*.gz ./*.pdf ./*.xmpi ./*.xmpdata
-latexmk -pdf thesis.tex
