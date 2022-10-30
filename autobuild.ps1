ce44100179d7fc3f2ed9570da9aa89a1b7bcf925
Emanuele Ballarin <emanuele@ballarin.cc>
2022-10-30 18:25:43 +0100

    Final update, post thesis defence
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- autobuild.ps1
+++ autobuild.ps1
@@ -2,8 +2,17 @@
 cd "$HOME/repositories/carso-msc-thesis"
 latexmk -C
 Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
-latexmk -interaction=nonstopmode -pdf thesis.tex
-pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
-latexmk -pdflatex=lualatex -interaction=nonstopmode -pdf slides.tex
-lualatex -synctex=1 -interaction=nonstopmode slides.tex
+if ($args -contains "thesis") {
+    latexmk -interaction=nonstopmode -pdf thesis.tex
+    pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
+}
+if ($args -contains "slides") {
+    latexmk -pdflatex=lualatex -interaction=nonstopmode -pdf slides.tex
+    lualatex -synctex=1 -interaction=nonstopmode slides.tex
+}
+if ($args -contains "thinslides") {
+    latexmk -pdflatex=lualatex -interaction=nonstopmode -pdf thin_slides.tex
+    lualatex -synctex=1 -interaction=nonstopmode thin_slides.tex
+}
+Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
 Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
--- cleanup.ps1
+++ cleanup.ps1
@@ -1,4 +1,3 @@
 #!/usr/bin/env pwsh
 cd "$HOME/repositories/carso-msc-thesis"
-latexmk -C
-Remove-Item * -Include *.aux,*.bbl,*.log,*.out,*.blg,*.toc,*.gz,*.pdf,*.xmpi,*.xmpdata,*.nav,*.snm,*.fls,*.fdb_latexmk
+& ./autobuild.ps1
